3f5ada940efb607ca12cd9ce45e5fce39ab4bcf7
jefferymason <52844021+jefferymason@users.noreply.github.com>
2021-08-17 13:54:18 -0500

    Add files via upload

---
+++ One File Bullseye/One File Bullseye/ContentView.swift
@@ -0,0 +1,307 @@
+//
+//  ContentView.swift
+//  One File Bullseye
+//
+//  Created by Jeff Mason on 8/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+  @State private var alertIsVisible = false
+  @State private var sliderValue = 50.0
+  @State private var game = Game()
+
+  var body: some View {
+    ZStack {
+        BackgroundView(game: $game)
+        VStack {
+            InstructionsView(game: $game)
+                .padding(.bottom, alertIsVisible ? 0 : 100)
+            if alertIsVisible {
+                PointsView(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
+                  .transition(.scale)
+            } else {
+                HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
+                  .transition(.scale)
+              }
+        }
+            if !alertIsVisible {
+              SliderView(sliderValue: $sliderValue)
+                .transition(.scale)
+        }
+    }
+  }
+}
+
+struct HitMeButton: View {
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+
+    var body: some View {
+        Button(action: {
+          withAnimation {
+            alertIsVisible = true
+          }
+        }) {
+            Text("HIT ME")
+                .bold()
+                .font(.title3)
+    }
+        .padding(20.0)
+        .background(
+          ZStack {
+              Color.blue
+              LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+          }
+        )
+        .foregroundColor(Color.white)
+        .cornerRadius(21.0)
+    }
+}
+
+struct InstructionsView: View {
+  @Binding var game: Game
+
+  var body: some View {
+    VStack {
+      InstructionText(text: "🎯🎯🎯\nPut the Bullseye as close as you can to")
+        .padding(.leading, 20.0)
+        .padding(.trailing, 20.0)
+      BigNumberText(text: String(game.target))
+    }
+  }
+}
+
+struct SliderView: View {
+  @Binding var sliderValue: Double
+
+  var body: some View {
+    HStack {
+      SliderLabelText(text: "1")
+      Slider(value: $sliderValue, in: 1.0...100.0)
+      SliderLabelText(text: "100")
+    }
+    .padding()
+  }
+}
+
+struct Game {
+    var target = Int.random(in: 1...100)
+    var score = 0
+    var round = 1
+    
+    func points(sliderValue: Int) -> Int {
+      100 - abs(target - sliderValue)
+    }
+    
+    mutating func startNewRound(points: Int) {
+        score += points
+        round += 1
+        target = Int.random(in: 1...100)
+    }
+    
+    mutating func restart() {
+        score = 0
+        round = 1
+        target = Int.random(in: 1...100)
+    }
+}
+
+struct BackgroundView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        VStack {
+            TopView(game: $game)
+            Spacer()
+            BottomView(game: $game)
+        }
+        .padding()
+    }
+}
+
+struct TopView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                game.restart()
+            }) {
+                RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+            }
+            Spacer()
+        }
+    }
+}
+
+struct NumberView: View {
+    var title: String
+    var text: String
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            LabelText(text: title)
+            RoundedRectTextView(text: text)
+        }
+    }
+}
+
+struct BottomView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        HStack {
+            NumberView(title: "SCORE", text: String(game.score))
+            Spacer()
+            NumberView(title: "ROUND", text: String(game.round))
+        }
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView(game: .constant(Game()))
+    }
+}
+
+struct PointsView: View {
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    
+    var body: some View {
+        let roundedValue = Int(sliderValue.rounded())
+        let points = game.points(sliderValue: roundedValue)
+        
+        VStack(spacing: 10) {
+            InstructionText(text: "The slider's value is")
+            BigNumberText(text: String(roundedValue))
+            BodyText(text: "You score \(points) points\n🎉🎉🎉")
+            Button(action: {
+                withAnimation {
+                    alertIsVisible = false
+                }
+                game.startNewRound(points: points)
+            }) {
+                ButtonText(text: "Start New Round")
+            }
+        }
+        .padding()
+        .frame(maxWidth: 300)
+        .background(Color("BackgroundColor"))
+        .cornerRadius(21.0)
+        .shadow(radius: 10, x:5, y:5)
+    }
+}
+
+struct InstructionText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .bold()
+            .kerning(2.0)
+            .multilineTextAlignment(.center)
+            .lineSpacing(4.0)
+            .font(.footnote)
+    }
+}
+
+struct BigNumberText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .kerning(-1.0)
+            .font(.largeTitle)
+            .fontWeight(.black)
+    }
+}
+
+struct SliderLabelText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .bold()
+            .frame(width: 35)
+    }
+}
+
+struct LabelText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .bold()
+            .kerning(1.5)
+            .font(.caption)
+    }
+}
+
+struct BodyText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .multilineTextAlignment(.center)
+            .lineSpacing(12.0)
+    }
+}
+
+struct ButtonText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .bold()
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.green)
+            .cornerRadius(12.0)
+    }
+}
+
+struct RoundedImageViewStroked: View {
+    var systemName: String
+    
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .frame(width: 56, height: 56)
+    }
+}
+
+struct RoundedRectTextView: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .kerning(-0.2)
+            .bold()
+            .font(.title3)
+            .frame(width: 68.0, height: 56.0)
+            .overlay(RoundedRectangle(cornerRadius: 21.0)
+                        .stroke(lineWidth: 2.0)
+                        .fill(Color(.lightGray)))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+    ContentView()
+      .previewLayout(.fixed(width: 568, height: 320))
+    ContentView()
+      .preferredColorScheme(.dark)
+    ContentView()
+      .preferredColorScheme(.dark)
+      .previewLayout(.fixed(width: 568, height: 320))
+  }
+}
---
+++ One File Bullseye/One File Bullseye/One_File_BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_File_BullseyeApp.swift
+//  One File Bullseye
+//
+//  Created by Jeff Mason on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct One_File_BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
